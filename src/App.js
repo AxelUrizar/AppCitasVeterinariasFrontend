a10c466d4b7ca09afc2c07775e8ab5835c6069f5
FakeFly <axelurizarmartin@gmail.com>
2022-02-21 19:48:46 +0000

    instalacion componente

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp"
 import NavBar from "./components/NavBar";
 import Perfil from "./components/Perfil";
-import Mascotas from "./components/Mascotas";
+import Mascotas from "./components/Mascota";
 import CitasMascota from "./components/CitasMascota";
 import Home from "./components/Home";
 import NuevaMascota from "./components/NuevaMascota";
+import TodasMascotas from "./components/TodasMascotas";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App vh-100">
-        <NavBar />
-        
-          <Routes>
-            {/* Paths Principales */}
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
+    <div className="App vh-100">
+      <NavBar />
+      
+        <Routes>
+          {/* Paths Principales */}
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
 
-            {/* Paths Usuarios */}
-            <Route path='/perfil' element={<Perfil />} />
-              {/* <Route path='/logout' element={<Logout />} /> */}
-              {/* <Route path='/logoutAll' element={<LogoutAll />} /> */}
-              {/* <Route path='/eliminarCuenta' element={<EliminarCuenta />} /> */}
+          {/* Paths Usuarios */}
+          <Route path='/perfil' element={<Perfil />} />
+            {/* <Route path='/logout' element={<Logout />} /> */}
+            {/* <Route path='/logoutAll' element={<LogoutAll />} /> */}
+            {/* <Route path='/eliminarCuenta' element={<EliminarCuenta />} /> */}
 
-            {/* Paths Mascotas */}
-              <Route path='/perfil/nuevaMascota' element={<NuevaMascota />} />
-            <Route path='/mascotas/:nombre' element={<Mascotas />} />
-            <Route path='/citas/:nombre' element={<CitasMascota />} />
+          {/* Paths Mascotas */}
+          <Route path='/perfil/nuevaMascota' element={<NuevaMascota />} />
+          <Route path='/mascotas/:nombre' element={<Mascotas />} />
+          <Route path='/citas/:nombre' element={<CitasMascota />} />
 
-          </Routes>
-      </div>
-    </BrowserRouter>
+          <Route path='/todasMascotas' element={<TodasMascotas />}/>
+
+        </Routes>
+    </div>  
   );
 }
 
